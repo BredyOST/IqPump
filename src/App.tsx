5fbfd92636bedfe5490b3e77c1b994c30e99bd1b
maksim <89292642644m@gmail.com>
2025-02-24 19:42:59 +0300

    добвил конфиг для app kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
+import { createAppKit } from '@reown/appkit/react';
+import {EthersAdapter} from "@reown/appkit-adapter-ethers";
+import { arbitrum, } from '@reown/appkit/networks'
+
+const projectId = process.env.REACT_APP_APP_KIT_ID;
+
+if (!projectId) {
+  throw new Error('REACT_APP_APP_KIT_ID is not defined in the environment variables');
+}
+
+// 3. Create a metadata object
+const metadata = {
+  name: 'My Website',
+  description: 'My Website description',
+  url: 'https://mywebsite.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.mywebsite.com/'],
+};
+
+// 4. Create a AppKit instance
+createAppKit({
+  adapters: [new EthersAdapter()],
+  networks: [arbitrum],
+  metadata,
+  projectId,
+  features: {
+    analytics: true, // Optional - defaults to your Cloud configuration
+    email: false, // default to true
+    socials: false,
+    swaps: false, // Optional - true by default
+    send: false,
+    onramp: false, // убрать buy cripto
+  },
+});
+
 
 function App() {
 
