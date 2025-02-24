8943eea66953401470abeb503a65618e2ebb792a
maksim <89292642644m@gmail.com>
2025-02-24 19:27:33 +0300

    инициализация приложения

---
+++ src/features/iqPumpService/iqPumpMainWindow.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import DonHaveAccount from '../donHaveAccount/donHaveAccount';
+import { useAppSelector } from '../../shared/redux/hooks/hooks';
+import HaveAccount from '../haveAccount/haveAccount';
+
+export const isHaveAccount = true;
+
+const IqPumpMainWindow = () => {
+    const { telegramUsername, loggedIn } = useAppSelector(({ authSlice }) => authSlice);
+
+    return (
+        <>
+            {/*<DonHaveAccount />*/}
+            {loggedIn && <HaveAccount />}
+        </>
+    );
+};
+
+export default IqPumpMainWindow;
