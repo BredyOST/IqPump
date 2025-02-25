137c106cdf663d60009ac2711b066a47e60c0d6a
maksim <89292642644m@gmail.com>
2025-02-25 19:43:05 +0300

    добавил компонент уведомлений

---
+++ src/shared/tostify/attention.ts
@@ -0,0 +1,17 @@
+import { toast } from 'react-toastify';
+
+export const showAttention = (message: string, indicator: 'error' | 'success' | 'warning' | 'info'): void => {
+    if (indicator === 'error') {
+        const notify = () => toast.error(message);
+        notify();
+    }
+    if (indicator === 'success') {
+        const notify = () => toast.success(message);
+        notify();
+    }
+
+    if (indicator === 'warning') {
+        const notify = () => toast.warning(message);
+        notify();
+    }
+};
