41779d3b1e181f6f0c65f224f723162c4d250762
maksim <89292642644m@gmail.com>
2025-03-03 01:26:07 +0300

    функция для теста входа

--- src/features/main-iq-pump-window/main-iq-pump-window.tsx
+++ src/features/main-iq-pump-window/main-iq-pump-window.tsx
@@ -100,38 +100,74 @@ const MainIqPumpWindow = observer(() => {
     // }
 
     async function connectAccount(): Promise<void> {
-        const timeout = 10000; // 10 секунд для тайм-аута
-        let timeoutReached = false;
-        console.log('1')
-        // Запускаем таймер
-        const timeoutId = setTimeout(() => {
-            timeoutReached = true;
-            console.log('Тайм-аут превышен');
-            // Здесь можно отправить уведомление
-            showAttention('Запрос подключения не был завершен вовремя', 'warning');
-            sendPushNotification('Ошибка подключения', 'Попробуйте подключиться снова.');
-        }, timeout);
 
-        try {
-            // Выполнение попытки подключения
-            await open();
+        console.log(1111)
 
-            // Если подключение прошло успешно, отменяем тайм-аут
-            if (!timeoutReached) {
-                clearTimeout(timeoutId);
-                console.log('Подключение успешно');
-            }
-        } catch (error) {
-            if (timeoutReached) {
-                console.log('Запрос был отменен по тайм-ауту');
-            } else {
-                console.log('Ошибка при подключении: ', error);
-            }
-        } finally {
-            // Очистка таймера, если подключение было успешным или ошибка была обработана
-            clearTimeout(timeoutId);
+        let signer = null;
+
+        let provider;
+        if (window.ethereum == null) {
+            console.log(2222)
+
+            // If MetaMask is not installed, we use the default provider,
+            // which is backed by a variety of third-party services (such
+            // as INFURA). They do not have private keys installed,
+            // so they only have read-only access
+            console.log("MetaMask not installed; using read-only defaults")
+            provider = ethers.getDefaultProvider()
+
+        } else {
+
+            // Connect to the MetaMask EIP-1193 object. This is a standard
+            // protocol that allows Ethers access to make all read-only
+            // requests through MetaMask.
+            provider = new ethers.BrowserProvider(window.ethereum)
+            console.log(provider)
+
+            // It also provides an opportunity to request access to write
+            // operations, which will be performed by the private key
+            // that MetaMask manages for the user.
+            signer = await provider.getSigner();
+            console.log(signer);
+            showAttention(`${signer.address}`, 'error')
+            console.log(signer);
         }
+
+
+        // const timeout = 10000; // 10 секунд для тайм-аута
+        // let timeoutReached = false;
+        // console.log('1')
+        // // Запускаем таймер
+        // const timeoutId = setTimeout(() => {
+        //     timeoutReached = true;
+        //     console.log('Тайм-аут превышен');
+        //     // Здесь можно отправить уведомление
+        //     showAttention('Запрос подключения не был завершен вовремя', 'warning');
+        //     sendPushNotification('Ошибка подключения', 'Попробуйте подключиться снова.');
+        // }, timeout);
+        //
+        // try {
+        //     // Выполнение попытки подключения
+        //     await open();
+        //
+        //     // Если подключение прошло успешно, отменяем тайм-аут
+        //     if (!timeoutReached) {
+        //         clearTimeout(timeoutId);
+        //         console.log('Подключение успешно');
+        //     }
+        // } catch (error) {
+        //     if (timeoutReached) {
+        //         console.log('Запрос был отменен по тайм-ауту');
+        //     } else {
+        //         console.log('Ошибка при подключении: ', error);
+        //     }
+        // } finally {
+        //     // Очистка таймера, если подключение было успешным или ошибка была обработана
+        //     clearTimeout(timeoutId);
+        // }
     }
+
+
 
 
     function sendPushNotification(title: string, message: string) {
