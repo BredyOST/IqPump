a7a0b4eff3a29110f32e158e0f32e8a13a698c5d
maksim <89292642644m@gmail.com>
2025-03-03 11:17:27 +0300

    изменил z index шапки

--- src/features/main-iq-pump-window/main-iq-pump-window.tsx
+++ src/features/main-iq-pump-window/main-iq-pump-window.tsx
@@ -101,39 +101,6 @@ const MainIqPumpWindow = observer(() => {
 
     async function connectAccount(): Promise<void> {
 
-
-
-        // let signer = null;
-        //
-        // let provider;
-        // if (window.ethereum == null) {
-        //     console.log(2222)
-        //
-        //     // If MetaMask is not installed, we use the default provider,
-        //     // which is backed by a variety of third-party services (such
-        //     // as INFURA). They do not have private keys installed,
-        //     // so they only have read-only access
-        //     console.log("MetaMask not installed; using read-only defaults")
-        //     provider = ethers.getDefaultProvider()
-        //
-        // } else {
-        //
-        //     // Connect to the MetaMask EIP-1193 object. This is a standard
-        //     // protocol that allows Ethers access to make all read-only
-        //     // requests through MetaMask.
-        //     provider = new ethers.BrowserProvider(window.ethereum)
-        //     console.log(provider)
-        //
-        //     // It also provides an opportunity to request access to write
-        //     // operations, which will be performed by the private key
-        //     // that MetaMask manages for the user.
-        //     signer = await provider.getSigner();
-        //     console.log(signer);
-        //     showAttention(`${signer.address}`, 'error')
-        //     console.log(signer);
-        // }
-
-
         const timeout = 10000; // 10 секунд для тайм-аута
         let timeoutReached = false;
         console.log('1')
